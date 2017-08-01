3a3ba0207f9fce9c2e024487be9391f37d51f5ac
kavinkumar06 <kavinm09@gmail.com>
2017-08-01 09:14:07 +0530

    Create Coins.java

---
+++ Coins.java
@@ -0,0 +1,34 @@
+# Coins
+import java.util.Scanner;
+
+public class CoinsCount {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner get=new Scanner(System.in);
+		System.out.println("Number of Coins Available");
+		int count=get.nextInt();
+		int arr[]=new int[count];
+		for(int i=0;i<count;i++)
+		{
+			arr[i]=get.nextInt();
+		}
+		System.out.println("Enter the TotalSum value");
+		int sum=get.nextInt();
+		int sub;
+		int coin=0;
+		while(sum!=0)
+		{
+			sub=0;
+			sub=sum/arr[count-1];
+			if(sub!=0)
+			{
+				System.out.println(sub+" Coin of "+arr[count-1]);
+			}
+				sum=coin+sum%arr[count-1];
+				count--;
+			}
+		}
+	
+}
